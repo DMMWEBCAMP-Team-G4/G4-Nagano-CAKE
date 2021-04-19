314cb45d38feaff1d6476f604fe28f2468d380c7
wad0 <fukushima0102@gmail.com>
2021-04-19 03:34:57 +0000

    ProductsController

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,9 +1,17 @@
 class Admin::ProductsController < ApplicationController
   
   def new
+    @product=Product.new
+  end
+  
+  def create
+    @product=Product.new(product_params)
+    @product.save
+    redirect_to admin_product_path(@product.id)
   end
 
   def index
+    @products=Product.all
   end
 
   def show
@@ -11,4 +19,11 @@ class Admin::ProductsController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def product_params
+    params.require(:product).permit(:image_id,:name,:description,:genre_id,:price,:is_active)
+  end
+  
 end
