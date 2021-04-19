9faec7b410ae364495a634c93838dbcd7d962921
wad0 <fukushima0102@gmail.com>
2021-04-19 03:18:10 +0000

    RoutesAndView

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
-class ProductsController < ApplicationController
+class Admin::ProductsController < ApplicationController
+  
   def new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,29 @@
 Rails.application.routes.draw do
- 
- 
+
+
 
   get 'homes/top'
 
   get 'homes/about'
 
   devise_for :admins
-  
+
   namespace :admin do
     resources :members
-    resources :products
+    resources :products,only: [:index,:new,:create,:show,:edit,:update]
     resources :genres
     resources :orders
     resources :order_products
   end
-    
+
   devise_for :members
-  
+
     resources :members
       root 'public/homes#top'
     resources :shipping_addresses
     resources :orders
-    resources :products
+    resources :products,only: [:index,:show]
     resources :cart_items
-    
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
