9db3be5ddc37ee136793abc27b373f75bf7ce60e
wad0 <fukushima0102@gmail.com>
2021-04-20 10:35:51 +0000

    new_view

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,9 +1,10 @@
 class Admin::ProductsController < ApplicationController
-  
+
   def new
     @product=Product.new
+    @genres=Genre.all
   end
-  
+
   def create
     @product=Product.new(product_params)
     @product.save
@@ -15,15 +16,16 @@ class Admin::ProductsController < ApplicationController
   end
 
   def show
+    @product=Product.find(params[:id])
   end
 
   def edit
   end
-  
+
   private
-  
+
   def product_params
     params.require(:product).permit(:image_id,:name,:description,:genre_id,:price,:is_active)
   end
-  
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+
+  belongs_to :genre
+  has_many :cart_items,dependent: :destroy
+  has_many :order_prodects,dependent: :destroy
+  attachment :image
+
 end
