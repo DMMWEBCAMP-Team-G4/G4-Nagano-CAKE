5100463b614e89c7cf8c1de60a6d03791b2419a4
koshi-jp <koushi0324@gmail.com>
2021-04-20 14:11:07 +0000

    createedit

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,12 +7,34 @@ class Admin::GenresController < ApplicationController
   end
   
   def create
+    @genre = Genre.new
+    if @genre.save
+       redirect_to admin_genres_path
+    end
+  end
+  
+  def show
+    @genre = Genre.find(params[:id])
   end
   
   def edit
+    @genre = Genre.find(params[:id])
   end
   
   def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to admin_genres_path
+    else
+      render :edit and return
+    end
+  end
+  
+  private
+  
+  def genre_params
+    params.require(:genre).permit(:name)
   end
   
 end
+
