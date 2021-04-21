b26d47cf6522ede337c05698d9af52edb6c7bd25
wad0 <fukushima0102@gmail.com>
2021-04-21 09:10:39 +0000

    new_show_edit_index

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,31 +1,39 @@
 class Admin::ProductsController < ApplicationController
 
   def new
-    @product=Product.new
-    @genres=Genre.all
+    @product = Product.new
+    @genres = Genre.all
   end
 
   def create
-    @product=Product.new(product_params)
+    @product = Product.new(product_params)
     @product.save
     redirect_to admin_product_path(@product.id)
   end
 
   def index
-    @products=Product.all
+    @products = Product.all
   end
 
   def show
-    @product=Product.find(params[:id])
+    @product = Product.find(params[:id])
   end
 
   def edit
+    @product = Product.find(params[:id])
+    @genres = Genre.all
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to admin_product_path(@product.id)
   end
 
   private
 
   def product_params
-    params.require(:product).permit(:image_id,:name,:description,:genre_id,:price,:is_active)
+    params.require(:product).permit(:image,:name,:description,:genre_id,:price,:is_active)
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,13 @@
 class Product < ApplicationRecord
 
   belongs_to :genre
+  # , optional: true
   has_many :cart_items,dependent: :destroy
-  has_many :order_prodects,dependent: :destroy
+  has_many :order_products,dependent: :destroy
   attachment :image
+
+  def tax_price
+      (self.price * 1.10).round
+  end
 
 end
