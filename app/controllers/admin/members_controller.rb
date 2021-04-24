b88815e8d7b4d77bd7f8bc03bb8f910b6dd81669
mikanfirst <mmkkmkk4869@gmail.com>
2021-04-24 13:09:29 +0000

    [add]会員一覧ページ

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,19 +1,20 @@
 class Admin::MembersController < ApplicationController
-  
+
   def index
+    @members = Member.all
   end
-  
+
   def show
     @member = Member.find(params[:id])
     @fullname = @member.last_name+@member.first_name
     @fullname_kana = @member.last_name_kana+@member.first_name_kana
   end
-  
+
   def edit
      @member = Member.find(params[:id])
      @fullname = @member.last_name+@member.first_name
   end
-  
+
   def update
      @member = Member.find(params[:id])
      if @member.update(member_params)
@@ -22,10 +23,10 @@ class Admin::MembersController < ApplicationController
       render :edit and return
      end
   end
-  
+
   private
 	def member_params
 	  params.require(:member).permit(:email,:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:phone_number,:member_status)
 	end
-  
+
 end
