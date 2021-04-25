9d7a5e085972969f9554f001bb311ebc4ae1ca19
mikanfirst <mmkkmkk4869@gmail.com>
2021-04-25 13:26:04 +0000

    [fix]管理者ログイン後遷移先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a?(Admin)
-      admin_products_path
+      admin_orders_path
     else
       root_path
     end
